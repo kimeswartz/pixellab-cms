ea64fbc0f8dcc4291424ba1c74b7c97c8bd6135e
kimeswartz <kim.e.swartz@gmail.com>
2024-11-11 13:52:45 +0100

    connection to maria db works as expected

---
+++ src/main/java/com/kim/pixellab_cms/PixellabCmsApplication.java
@@ -0,0 +1,13 @@
+package com.kim.pixellab_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixellabCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixellabCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kim/pixellab_cms/PixellabCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.pixellab_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixellabCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
